bbd04a126fa531d91ad80bdd582ed129de457bdf
hushell <dom343@gmail.com>
2017-09-02 13:32:25 +0200

    improved camvid dataloader

--- data/camvid_dataset.py
+++ data/camvid_dataset.py
@@ -1,12 +1,13 @@
 import os
 import collections
 import torch
+import torch.utils.data as data
 import torchvision
+import torchnet as tnt
 import numpy as np
 import scipy.misc as m
-import matplotlib.pyplot as plt
-
-from torch.utils import data
+import util
+import cv2
 
 
 class CamvidDataset(data.Dataset):
@@ -16,10 +17,45 @@ class CamvidDataset(data.Dataset):
         self.heightSize = 360
         self.widthSize = 480
         self.n_classes = 12
-        self.mean = np.array([104.00699, 116.66877, 122.67892])
-        #self.files = collections.defaultdict(list)
+        #self.mean = np.array([104.00699, 116.66877, 122.67892])
+        self.mean = [0.41189489566336, 0.4251328133025, 0.4326707089857]
+        self.std = [0.27413549931506, 0.28506257482912, 0.28284674400252]
         self.files = os.listdir(root + '/' + self.split)
 
+        # transforms
+        transform_list = []
+        transform_img = tnt.transform.compose([
+            #lambda x: x[:,:,::-1], # RGB->BGR
+            lambda x: x.transpose(2,0,1).astype(np.float32),
+            lambda x: torch.from_numpy(x).div_(255.0),
+            torchvision.transforms.Normalize(mean = self.mean, std = self.std),
+        ])
+
+        transform_target = tnt.transform.compose([
+            lambda x: x.astype(np.int32),
+            torch.from_numpy,
+            lambda x: x.contiguous(),
+            #lambda x: x.view(1,x.size(0), x.size(1)), # HxW -> 1xHxW
+        ])
+
+        if opt.isTrain:
+            interp_img = cv2.INTER_LINEAR
+            interp_target = cv2.INTER_NEAREST
+
+            if 'resize' in opt.resize_or_crop:
+                target_scale = float(opt.targetSize) / float(max(opt.widthSize, opt.heightSize))
+                transform_list.append(util.Scale(target_scale, interp_img, interp_target))
+
+            if 'crop' in opt.resize_or_crop:
+                transform_list.append(util.RandomCrop(crop_width=opt.widthSize, crop_height=opt.heightSize))
+
+            if not opt.no_flip:
+                transform_list.append(util.RandomFlip())
+
+        transform_list.append(util.ImgTargetTransform(img_transform=transform_img, target_transform=transform_target))
+        self.transform_fun = tnt.transform.compose(transform_list)
+
+        # visualization
         self.label2color = np.array([(128, 128, 128),
                                      (128, 0, 0),
                                      (192, 192, 128),
@@ -54,7 +90,8 @@ class CamvidDataset(data.Dataset):
         lbl = m.imread(lbl_path)
         lbl = np.array(lbl, dtype=np.int32)
 
-        img, lbl = self.transform(img, lbl)
+        #img, lbl = self.transform(img, lbl)
+        img, lbl = self.transform_fun((img, lbl))
 
         return {'A': img, 'B': lbl,
                 'A_paths': img_path, 'B_paths': lbl_path}
